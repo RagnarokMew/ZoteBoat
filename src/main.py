f93e3c17c35b7a6bbbad7584740eb2fd963c5da8
RagnarokMew <ragnarokmewdev@gmail.com>
2025-10-29 00:28:11 +0200

    feat: add game entrypoint

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import arcade
+from core.constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
+from core import game
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = game.GameView()
+    window.show_view(start_view)
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
