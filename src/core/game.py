40a5126b213302eb9766eb36a6399bd51227cee2
RagnarokMew <ragnarokmewdev@gmail.com>
2025-10-29 00:15:25 +0200

    feat(core): add GameView skeleton

---
+++ src/core/game.py
@@ -0,0 +1,15 @@
+import arcade
+
+class GameView(arcade.View):
+    
+    def __init__(self):
+        super().__init__()
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        self.clear()
+
+    def on_update(self, delta_time):
+        pass
