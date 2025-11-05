ae262c65f3a4ec87bea120616f922a908486c1d2
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-05 17:39:59 +0200

    feat(entities): add parent sprite tracking to player attack

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,4 +1,5 @@
 import arcade
+from core.constants import P_ATTACK_COOLDOWN, P_ATTACK_DAMAGE, P_HEALTH
 
 class PlayerSprite(arcade.Sprite):
 
@@ -13,6 +14,7 @@ class PlayerSprite(arcade.Sprite):
         self.center_x, self.center_y = position
         self.player_attack = None
         self.attack_cooldown = 0.0
+        self.health = P_HEALTH
         # TODO: Add textures for player (idle, walk, etc)
         # TODO: Should also work on logic to handle direction facing etc
         # The implementation of these features can be done later on
@@ -24,39 +26,47 @@ class PlayerSprite(arcade.Sprite):
         if(self.attack_cooldown > 0):
             return
 
+        a_x, a_y = self.position
+        a_x += 100
+        a_y += 10
+
         self.player_attack = PlayerAttack(
             self.scene,
-            self.position
+            self
         )
 
-        self.attack_cooldown = 0.5
+        self.attack_cooldown = P_ATTACK_COOLDOWN
 
     def update(self, delta_time):
         if(self.attack_cooldown > 0):
             self.attack_cooldown -= delta_time
 
         if(self.player_attack != None):
+            self.player_attack.position = self.position
             self.player_attack.update(delta_time)
 
 
 class PlayerAttack(arcade.Sprite):
 
-    def __init__(self, scene, position, scale=1.0, damage=1):
+    def __init__(self, scene, parent, scale=1.0, damage=P_ATTACK_DAMAGE):
         super().__init__(
-            ":resources:/images/tiles/lockRed.png",
+            # TODO: Change temp sprite with one of our own
+            ":resources:/onscreen_controls/flat_dark/right.png",
             scale=scale
         )
 
+        self.parent = parent
         self.scene = scene
-        self.center_x, self.center_y = position
         self.damage = damage
-        self.remaining_duration = 0.5
+        self.remaining_duration = P_ATTACK_COOLDOWN
 
         self.scene.add_sprite("PlayerAttack", self)
 
     def update(self, delta_time):
         # TODO: Update position based on Player
         self.remaining_duration -= delta_time
+        self.center_x = self.parent.center_x + 32
+        self.center_y = self.parent.center_y
 
         if(self.remaining_duration <= 0):
             self.remove_from_sprite_lists()
