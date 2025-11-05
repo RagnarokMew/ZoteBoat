5aee180095a09ce5c9be22d8c3d81ae6a58fbc53
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-05 16:15:58 +0200

    feat(entities): implement player attack stub

--- src/entities/player.py
+++ src/entities/player.py
@@ -2,20 +2,54 @@ import arcade
 
 class PlayerSprite(arcade.Sprite):
 
-    def __init__(self, position=(0, 0), scale=1.0):
+    def __init__(self, scene, position=(0, 0), scale=1.0):
         # TODO: Change temp asset to one of our own towards the end of development
         super().__init__(
             ":resources:images/animated_characters/robot/robot_idle.png",
             scale=scale
         )
 
+        self.scene = scene
         self.center_x, self.center_y = position
-
+        self.player_attack = None
         # TODO: Add textures for player (idle, walk, etc)
         # TODO: Should also work on logic to handle direction facing etc
         # The implementation of these features can be done later on
 
     # TODO: Should also work on logic to handle direction facing etc
     # The implementation of these features can be done later on
+
+    def attack(self):
+        self.player_attack = PlayerAttack(
+            self.scene,
+            self.position
+        )
+
+    def update(self, delta_time):
+        if(self.player_attack != None):
+            self.player_attack.update(delta_time)
+
+
+class PlayerAttack(arcade.Sprite):
+
+    def __init__(self, scene, position, scale=1.0, damage=1):
+        super().__init__(
+            ":resources:/images/tiles/lockRed.png",
+            scale=scale
+        )
+
+        self.scene = scene
+        self.center_x, self.center_y = position
+        self.damage = damage
+        self.remaining_duration = 0.5
+
+        self.scene.add_sprite("PlayerAttack", self)
+
+    def update(self, delta_time):
+        # TODO: Update position based on Player
+        self.remaining_duration -= delta_time
+
+        if(self.remaining_duration <= 0):
+            self.remove_from_sprite_lists()
 
 
