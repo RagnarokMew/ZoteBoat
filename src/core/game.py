b8ab242953bdaac68f5ece233d3aa9e70c6f3500
Luxaks <lucasciuca@gmail.com>
2025-11-08 14:28:21 +0200

    feat(core): basic scene loader

--- src/core/game.py
+++ src/core/game.py
@@ -1,7 +1,9 @@
 import arcade
+import time
 from entities import player
 from core.constants import GRAVITY, LEFT_FACING, PLAYER_MOVEMENT_SPEED, PLAYER_JUMP_SPEED, RIGHT_FACING, TILE_SCALING, UP_FACING, DOWN_FACING, SIDE_FACING
 from core.player_stats import PlayerStats
+from core.maps import map_array
 
 class GameView(arcade.View):
     
@@ -13,6 +15,7 @@ class GameView(arcade.View):
         self.player_texture = None
         self.player_sprite = None
         self.player_stats = None
+        self.player_trans_x = 0
 
         self.tile_map = None
         self.scene = None
@@ -28,18 +31,16 @@ class GameView(arcade.View):
         self.right_pressed = False
         self.left_pressed = False
 
-    def setup(self):
+    def setup(self, map_index):
         # Temporary tile map for stub creation
-        temp_map_name = ":resources:tiled_maps/map2_level_1.json"
+        self.map_index = map_index
         self.tile_map = arcade.load_tilemap(
-            temp_map_name,
-            scaling=TILE_SCALING
+            map_array[self.map_index],
+            scaling = TILE_SCALING
         )
 
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
-
         self.scene.add_sprite_list_after("Player", "Foreground")
-
         self.player_stats = PlayerStats()
 
         # Temporary Spawn, in the future it should be based on the map
@@ -48,6 +49,8 @@ class GameView(arcade.View):
             self.scene,
             temp_spawn
         )
+        # restore player speed after transition
+        self.player_sprite.change_x = self.player_trans_x
 
         self.scene.add_sprite("Player", self.player_sprite)
 
@@ -88,6 +91,10 @@ class GameView(arcade.View):
         if key == arcade.key.RIGHT:
             self.right_pressed = False
             self.player_sprite.change_x -= PLAYER_MOVEMENT_SPEED
+        
+        # temp manual map switch (debug)
+        if key == arcade.key.W:
+            self.change_map()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.Z:
@@ -121,3 +128,22 @@ class GameView(arcade.View):
         self.physics_engine.update()
         self.player_sprite.update(delta_time)
         self.camera.position = self.player_sprite.position
+
+        # if load zone doesn't exist in current scene, crashes, so check first
+        # (although all scenes should have a load zone eventually)
+        if "Load Zone" in self.scene:
+            loadzone = arcade.check_for_collision_with_list(
+                self.player_sprite,
+                self.scene["Load Zone"]
+            )
+            # if a loadzone was collided with, change scene
+            if loadzone:
+                self.change_map()
+    
+    # scene change handler
+    def change_map(self):
+    # since movement changes only on key press/release (not while holding),
+    # we must save the player speed before the transition
+        self.player_trans_x = self.player_sprite.change_x
+    # then change the map (actual map list/dict wip)
+        self.setup(1 - self.map_index)
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from core import game
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     start_view = game.GameView()
-    start_view.setup()
+    start_view.setup(0)
     window.show_view(start_view)
     arcade.run()
 
