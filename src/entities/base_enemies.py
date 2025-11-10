8febc786449ff3bf4723f464f7af458ddcef704f
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-10 12:46:43 +0200

    refactor(entities): moves repetitive enemy logic to a base class

--- src/entities/base_enemies.py
+++ src/entities/base_enemies.py
@@ -1,17 +1,41 @@
 import arcade
+from arcade.color import BLACK
 from core.constants import GRAVITY
 
-class GroundEnemy(arcade.Sprite):
-    def __init__(self, scene, path=":resources:/images/enemies/slimePurple.png", scale=1, damage=1, max_health=1, position=(128,128)):
+class BaseEnemy(arcade.Sprite):
+    def __init__(self, scene, sprite_path, position=(128, 128), scale=1, damage=1, max_health=1):
         super().__init__(
-            path,
+            sprite_path,
             scale=scale
         )
 
         self.center_x, self.center_y = position
         self.scene = scene
+        self.damage = damage
         self.max_health = max_health
         self.health = max_health
+        self.hp_text = arcade.Text(
+            f"{self.health}/{self.max_health}",
+            x=0,
+            y=0,
+            color=arcade.color.BLACK,
+            font_size=10
+        )
+
+    def update_text(self):
+        self.hp_text.text = f"{self.health}/{self.max_health}"
+
+class GroundEnemy(BaseEnemy):
+    def __init__(self, scene, sprite_path=":resources:/images/enemies/slimePurple.png", scale=1, damage=1, max_health=1, position=(128,128)):
+        super().__init__(
+            scene,
+            sprite_path,
+            scale=scale,
+            max_health=max_health,
+            position=position,
+            damage=damage
+        )
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self, scene["Platforms"], GRAVITY
         )
@@ -22,17 +46,18 @@ class GroundEnemy(arcade.Sprite):
         if(self.health <= 0):
             pass
 
-class FlyingEnemy(arcade.Sprite):
-    def __init__(self, scene, path=":resources:/images/enemies/bee.png", scale=1, damage=1, max_health=1, position=(128,128)):
+class FlyingEnemy(BaseEnemy):
+    def __init__(self, scene, sprite_path=":resources:/images/enemies/bee.png", scale=1, damage=1, max_health=1, position=(128,128)):
         super().__init__(
-            path,
-            scale=scale
+            scene,
+            sprite_path,
+            scale=scale,
+            max_health=max_health,
+            position=position,
+            damage=damage
+
         )
 
-        self.center_x, self.center_y = position
-        self.scene = scene
-        self.max_health = max_health
-        self.health = max_health
         self.physics_engine = arcade.PhysicsEngineSimple(
             self, scene["Platforms"]
         )
