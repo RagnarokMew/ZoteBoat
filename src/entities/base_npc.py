11bdc49bfdd12247d689cfaba541ce4b50a87c63
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-18 22:55:06 +0200

    feat(entities): add npc title and name to dialogue

--- src/entities/base_npc.py
+++ src/entities/base_npc.py
@@ -1,38 +1,99 @@
 import arcade
+from pyglet.graphics import Batch
 from core.constants import GRAVITY, SCREEN_HEIGHT, SCREEN_WIDTH
 
 class BaseNpc(arcade.Sprite):
-    def __init__(self, scene, sprite_path, position=(128, 128), scale=1):
+    def __init__(self, scene, sprite_path=":resources:/images/animated_characters/male_person/malePerson_idle.png", position=(128, 128), scale=1, name="NPC", title="Title"):
         super().__init__(
             sprite_path,
             scale=scale
         )
 
+        self.name = name
+        self.title = title
         self.scene = scene
         self.center_x, self.center_y = position
-        self.menu = None
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self, scene["Platforms"], GRAVITY
         )
-
-    def add_menu(self, Menu):
-        self.menu = Menu
-        self.scene.add_sprite("box", self.menu)
-
-    def draw(self):
-        if self.menu:
-            self.menu.draw()
 
     def update(self, delta_time):
         self.physics_engine.update()
 
 class DialogueMenu():
     def __init__(self,
-                 content=["This is a dialogue text", "Another text"]
+                 content=["This is a dialogue text", "Another text"],
+                 npc_name="NPC_NAME",
+                 npc_title="NPC_TITLE"
                  ):
         self.content = content
         self.text = None
         self.text_index = 0
+
+        # NOTE: Due to the amount of text that needs to be rendered here I've 
+        # decided to batch it in case it improves performance (at the cost of 
+        # a little RAM)
+        self.batch = Batch()
+        outlines = [
+            (0, 2),
+            (2, 0),
+            (-2, 0),
+            (0, -2),
+            (2, 2),
+            (-2, -2),
+            (2, -2),
+            (-2, 2)
+        ]
+
+        self.npc_text_shadow = list()
+        for outline in outlines:
+            self.npc_text_shadow.append(arcade.Text(
+                npc_name,
+                x=100 + outline[0],
+                y=100 + outline[1],
+                color=arcade.color.BLACK,
+                align="left",
+                bold=True,
+                font_size=32,
+                batch=self.batch
+            ))
+
+        self.npc_name_text = arcade.Text(
+            npc_name,
+            x=100,
+            y=100,
+            color=arcade.color.WHITE,
+            align="left",
+            bold=True,
+            font_size=32,
+            batch=self.batch
+        )
+
+        for outline in outlines:
+            self.npc_text_shadow.append(arcade.Text(
+                npc_title,
+                x=100 + outline[0],
+                y=50 + outline[1],
+                color=arcade.color.BLACK,
+                align="left",
+                bold=True,
+                italic=True,
+                font_size=28,
+                batch=self.batch
+            ))
+
+
+        self.npc_title_text = arcade.Text(
+            npc_title,
+            x=100,
+            y=50,
+            color=arcade.color.WHITE,
+            align="center",
+            bold=True,
+            italic=True,
+            font_size=28,
+            batch=self.batch
+        )
 
         self.next()
 
@@ -46,12 +107,13 @@ class DialogueMenu():
                 font_size=14,
                 width=SCREEN_WIDTH * 2 // 3 - 48,
                 align="left",
-                multiline=True
+                multiline=True,
+                batch=self.batch
             )
             self.text_index += 1
             return True
         except:
-            self.text = "Exception"
+            self.text = "Ok, bye"
             return False
 
     def draw(self):
@@ -59,5 +121,6 @@ class DialogueMenu():
             arcade.rect.XYWH(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 4 // 5, SCREEN_WIDTH * 2 // 3, SCREEN_HEIGHT // 4),
             arcade.color.BLACK
         )
-        self.text.draw()
+
+        self.batch.draw()
 
