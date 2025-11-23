b6fe55437ea88ed614e6c627eadc55779032f159
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-23 23:47:01 +0200

    feat(core): add currencies to player stats

--- src/core/player_stats.py
+++ src/core/player_stats.py
@@ -8,7 +8,10 @@ class PlayerStats():
         self.damage = P_ATTACK_DAMAGE
         self.max_inv_time = P_INV_TIME
         self.inv_time = P_INV_TIME
-        
+        self.currency_1 = 0
+        self.currency_2 = 0
+        self.currency_3 = 0
+        self.currency_4 = 0
         # TODO: Add other player related values
         # (e.g: has_double_jump, has_wall_jump, ...)
 
