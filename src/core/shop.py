79081f985e6776cab6836be9fe510b071f698f28
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-26 11:45:16 +0200

    feat(core): add shop menu text layout

--- src/core/shop.py
+++ src/core/shop.py
@@ -13,19 +13,102 @@ class ShopMenu:
     def __init__(self, items, player_stats, title):
         self.batch = Batch()
         self.items = items
+        self.active_index = 0
+        self.handler = ShopHandler(player_stats)
+
         self.title = arcade.Text(
             title,
-            x=SCREEN_WIDTH // 2 - len(title) * 9, # Hardcooded centering the text
+            x=SCREEN_WIDTH // 2,
             y=SCREEN_HEIGHT * 5 // 6 - 6,
             color=arcade.color.WHITE,
-            align="left",
             bold=True,
+            anchor_x="center",
             font_size=18,
             batch=self.batch
         )
-        self.handler = ShopHandler(player_stats)
+
+        self.instructions = arcade.Text(
+            "Z = Buy, X = Exit, UP/DOWN = Navigation between items",
+            x=SCREEN_WIDTH // 2 - SCREEN_WIDTH * 3 // 4 // 2 // 2 + 40,
+            y=SCREEN_HEIGHT // 6,
+            color=arcade.color.WHITE,
+            font_size=12,
+            anchor_x="center",
+            batch=self.batch
+        )
+
+        # NOTE: Below are the Text names of the "scrollable" items
+
+        self.prev_item_text = arcade.Text(
+            "ZoteBoat The Biography",
+            x = SCREEN_WIDTH // 2 - SCREEN_WIDTH * 3 // 4 // 2 // 2 + 40,
+            y = SCREEN_HEIGHT // 2 + SCREEN_HEIGHT * 3 // 4 // 4 + 10,
+            color=arcade.color.WHITE,
+            font_size=14,
+            anchor_y="center",
+            anchor_x="center",
+            batch=self.batch
+        )
+
+        self.active_item_text = arcade.Text(
+            "ZoteBoat The Biography",
+            x = SCREEN_WIDTH // 2 - SCREEN_WIDTH * 3 // 4 // 2 // 2 + 40,
+            y = SCREEN_HEIGHT // 2,
+            color=arcade.color.WHITE,
+            font_size=18,
+            anchor_y="center",
+            anchor_x="center",
+            batch=self.batch
+        )
+
+        self.next_item_text = arcade.Text(
+            "ZoteBoat The Biography",
+            x = SCREEN_WIDTH // 2 - SCREEN_WIDTH * 3 // 4 // 2 // 2 + 40,
+            y = SCREEN_HEIGHT // 2 - SCREEN_HEIGHT * 3 // 4 // 4 - 10,
+            color=arcade.color.WHITE,
+            font_size=14,
+            anchor_y="center",
+            anchor_x="center",
+            batch=self.batch
+        )
+
+        # NOTE: Below are the stats of the active (selected) item
+
+        self.item_name_text = arcade.Text(
+            "ZoteBoat The Biography",
+            x = SCREEN_WIDTH // 2 + SCREEN_WIDTH * 3 // 4 // 2 // 2 + 20,
+            y = SCREEN_HEIGHT * 3 // 4,
+            color = arcade.color.WHITE,
+            bold = True,
+            font_size=16,
+            anchor_x="center",
+            batch=self.batch
+        )
+
+        self.item_desc_text = arcade.Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            x = SCREEN_WIDTH // 2 + SCREEN_WIDTH * 3 // 4 // 2 // 2 + 20,
+            y = SCREEN_HEIGHT * 3 // 4 - 16 * 3,
+            color = arcade.color.WHITE,
+            font_size=12,
+            italic=True,
+            anchor_x="center",
+            width=SCREEN_WIDTH * 3 // 4 // 2 - 120 - 12 - 12 - 12,
+            multiline=True,
+            batch=self.batch
+        )
+
+        self.item_price_text = arcade.Text(
+            "PRICE: 1500 currency1",
+            x = SCREEN_WIDTH // 2 + SCREEN_WIDTH * 3 // 4 // 4 // 2 - 16,
+            y = SCREEN_HEIGHT // 4 - 16,
+            color = arcade.color.WHITE,
+            font_size=16,
+            batch=self.batch
+        )
 
         # TODO: go through items and check if player owns it already
+        # TODO: Set up Text strings based on shop items and active item
 
     def draw(self):
         # Main Shop Window
@@ -64,7 +147,7 @@ class ShopMenu:
                 SCREEN_WIDTH * 3 // 4 // 2 - 120,
                 SCREEN_HEIGHT * 3 // 4 // 4 // 1.5
             ),
-            arcade.color.GREEN
+            arcade.color.BLUE
         )
 
         # Active Item Window
@@ -79,7 +162,6 @@ class ShopMenu:
         )
 
         self.batch.draw()
-
 
 class ShopHandler:
     def __init__(self, player_stats):
