4ad266d3ae07f873d17720408bd317d76247c041
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-26 21:47:02 +0200

    feat(core): add item unlocks and related functions to player stats

--- src/core/player_stats.py
+++ src/core/player_stats.py
@@ -12,6 +12,29 @@ class PlayerStats():
         self.currency_2 = 0
         self.currency_3 = 0
         self.currency_4 = 0
+        self.unlocks = {
+            "Mask_1": False,
+            "Mask_2": False,
+            "Mask_3": False,
+            "Mask_4": False,
+            "Nail_Upgrade_Kit_1": False,
+            "Nail_Upgrade_Kit_2": False, # Each nail upgrade x2 DMG
+            "Wall_Jump": False,
+            "Double_Jump": False,
+            "Dash": False
+        }
         # TODO: Add other player related values
         # (e.g: has_double_jump, has_wall_jump, ...)
 
+    def increase_max_hp(self, amount):
+        self.max_health += amount
+        self.health = self.max_health
+
+    def increase_damage(self, amount):
+        self.damage += amount
+
+    def mark_unlocked(self, upgrade):
+        self.unlocks[upgrade] = True
+
+    def mark_locked(self, upgrade):
+        self.unlocks[upgrade] = False
