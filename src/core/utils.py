0bff1148525d8aae834527e732327a99d3882c6c
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-28 17:35:04 +0200

    feat(core): add shop items loading in utils

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,5 +1,6 @@
 import arcade
 from entities.base_npc import BaseNpc
+from core.shop import ShopItem
 import json
 
 def load_npc(id, scene, position):
@@ -20,7 +21,7 @@ def load_npc(id, scene, position):
             )
     except Exception as e:
         npc = BaseNpc(scene)
-        print(f"{e}")
+        print(f"Error Load Npc: {e}")
     finally:
         scene.add_sprite("NPC", npc)
         print(position)
@@ -30,9 +31,38 @@ def load_dialogue(id):
         with open("../assets/data/dialogue.json", "r") as file:
             data = json.load(file)
 
-        print(data[id])
         return data[id]
     except Exception as e:
-        print(f"{e}")
+        print(f"Error Load Dialogue: {e}")
         return ["Sorry, I got nothing to say."]
 
+def load_shop_items(id):
+    try:
+        with open("../assets/data/shops.json", "r") as shop_file:
+            item_ids = json.load(shop_file)
+            item_ids = item_ids[id]
+
+        with open("../assets/data/items.json", "r") as items_file:
+            data = json.load(items_file)
+
+        items = []
+
+        for id in item_ids:
+            items.append(ShopItem(
+                id=id,
+                name=data[id]["display_name"],
+                currency=data[id]["currency"],
+                price=data[id]["price"],
+                description=data[id]["description"]
+            ))
+    except Exception as e:
+        print(f"Error Load Shop Items: {e}")
+        items = [ShopItem(
+            name="No more items left...",
+            id="Null",
+            currency="?",
+            price=0,
+            description="Wow! You've bought everything my shop had to offer. Thank you so much Zote! I'll make sure to go and deposit all this currency at the nearest bank as soon as possible."
+        )]
+    finally:
+        return items
