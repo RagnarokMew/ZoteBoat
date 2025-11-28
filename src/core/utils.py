2f5e0edd7a566477377d92e3068d85524d523797
RagnarokMew <ragnarokmewdev@gmail.com>
2025-11-28 15:28:28 +0200

    feat(core): add npc loading function in utils

---
+++ src/core/utils.py
@@ -0,0 +1,27 @@
+import arcade
+from entities.base_npc import BaseNpc
+import json
+
+def load_npc(id, scene, position):
+    try:
+        base_path = "../assets/sprites/"
+        with open("../assets/data/npcs.json", "r") as file:
+            data = json.load(file)
+
+        npc = BaseNpc(
+            scene = scene,
+            id = id,
+            sprite_path = f"{base_path}{data[id]["sprite_path"]}",
+            position = position,
+            scale = data[id]["scale"],
+            name = data[id]["name"],
+            title = data[id]["title"],
+            has_shop = data[id]["has_shop"],
+            )
+    except Exception as e:
+        npc = BaseNpc(scene)
+        print(f"{e}")
+    finally:
+        scene.add_sprite("NPC", npc)
+        print(position)
+
