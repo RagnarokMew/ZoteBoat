92fcf58c989cbc6c4a9f58291352837fe2ea1ffa
Luxaks <lucasciuca@gmail.com>
2025-11-29 20:06:45 +0200

    fix: rename load zone tileset to match branch 'sprites'

--- assets/tilesets/load.tsx
+++ assets/tilesets/load.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="loadzone" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="load" tilewidth="64" tileheight="64" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
