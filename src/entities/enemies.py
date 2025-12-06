83de4bded066a7376af893a43a7ca0cf6f739e0d
RagnarokMew <ragnarokmewdev@gmail.com>
2025-12-06 17:44:21 +0200

    feat(core): add ground chaser enemy ai

--- src/entities/enemies.py
+++ src/entities/enemies.py
@@ -62,7 +62,40 @@ class ChaserGround(GroundEnemy):
                     drop_curr4=drop_curr4,
                     frame_duration=frame_duration
                 )
+        self.target = target
+        self.movement_speed = speed
+        self.change_x = self.movement_speed
+        self.wander_range = wander_range
 
+    def update(self, delta_time):
+        self.update_ai()
+
+        if self.ai_state == "chase":
+            if self.center_x < self.target.center_x:
+                self.change_x = self.movement_speed
+            else:
+                self.change_x = -self.movement_speed
+
+        elif self.ai_state == "idle":
+            if self.center_x > self.spawn_x + self.wander_range and self.change_x >= 0:
+                self.change_x = -self.movement_speed
+            elif self.center_x < self.spawn_x - self.wander_range and self.change_x <= 0:
+                self.change_x = self.movement_speed
+
+        super().update(delta_time)
+
+    def update_ai(self):
+        if self.target == None:
+            self.ai_state == "idle"
+            return
+
+        target_distance = abs(self.target.center_x - self.center_x)
+
+        # Aggro range is currently set to the wander_range
+        if target_distance < self.wander_range * 2:
+            self.ai_state = "chase"
+        else:
+            self.ai_state = "idle"
 
 class IdleFlying(FlyingEnemy):
     def __init__(self, scene, sprite_path, target, speed, wander_range,
