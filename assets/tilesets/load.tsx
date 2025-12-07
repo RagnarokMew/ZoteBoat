e37ad437002023542de9376496ac37fd6ec37f7e
Luxaks <lucasciuca@gmail.com>
2025-12-07 23:50:24 +0200

    feat: parkour maps

--- assets/tilesets/load.tsx
+++ assets/tilesets/load.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="load" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="load" tilewidth="64" tileheight="64" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -18,5 +18,11 @@
    <property name="map_id" value="parkour_01"/>
   </properties>
   <image source="../sprites/icons/blue_check.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="map_id" value="respawn"/>
+  </properties>
+  <image source="../sprites/icons/red_cross.png" width="64" height="64"/>
  </tile>
 </tileset>
